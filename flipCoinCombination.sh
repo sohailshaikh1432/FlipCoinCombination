cbfdd1b45d46c42864a91746aace272bfdaf1e83
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 04:06:47 +0530

    Flip a coin multiple times, Determine percentage of the Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
-coinFlip=$((RANDOM%2))
-if [ $coinFlip -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A coinDictionary
+coinDictionary[Heads]=0
+coinDictionary[Tails]=0
+
+for (( i=0 ; i<10 ; i++ ))
+do
+	coinFlip=$((RANDOM%2))
+	if [ $coinFlip -eq 0 ]
+	then
+		((coinDictionary[Heads]++))
+	else
+		((coinDictionary[Tails]++))
+	fi
+done
+echo "Percentage of heads is $((${coinDictionary[Heads]}*100/10))%"
+echo "Percentage of tails is $((${coinDictionary[Tails]}*100/10))%"
