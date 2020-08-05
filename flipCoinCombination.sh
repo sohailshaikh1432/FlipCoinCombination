e68b8a1736f2e2415c45137f4554a5abd0098d15
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 04:09:44 +0530

    Flip a coin multiple times, Determine percentage of the Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -x
 
 declare -A coinDictionary
-coinDictionary[Heads]=0
-coinDictionary[Tails]=0
+coinDictionary[HH]=0
+coinDictionary[HT]=0
+coinDictionary[TH]=0
+coinDictionary[TT]=0
 
 for (( i=0 ; i<10 ; i++ ))
 do
 	coinFlip=$((RANDOM%2))
-	if [ $coinFlip -eq 0 ]
-	then
-		((coinDictionary[Heads]++))
-	else
-		((coinDictionary[Tails]++))
-	fi
+	coinFlip2=$((RANDOM%2))
+	case $coinFlip$coinFlip2 in
+		00)
+			((coinDictionary[HH]++))
+			;;
+		01)
+			((coinDictionary[HT]++))
+			;;
+		10)
+			((coinDictionary[TH]++))
+			;;
+		11)
+			((coinDictionary[TT]++))
+			;;
+	esac
 done
-echo "Percentage of heads is $((${coinDictionary[Heads]}*100/10))%"
-echo "Percentage of tails is $((${coinDictionary[Tails]}*100/10))%"
+echo "Percentage of two consecutive heads is $((${coinDictionary[HH]}*100/10))%"
+echo "Percentage of heads tails is $((${coinDictionary[HT]}*100/10))%"
+echo "Percentage of tails heads is $((${coinDictionary[TH]}*100/10))%"
+echo "Percentage of two consecutive tails is $((${coinDictionary[TT]}*100/10))%"
